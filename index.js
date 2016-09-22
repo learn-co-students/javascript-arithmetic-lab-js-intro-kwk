7bf647b3163113e94856ae880b10e223816d8b5c
Victoria Chiu <victoria@chiucompany.com>
2016-09-22 00:12:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 function add(a, b) {
-  var a = (1);
-  var b = (80);
-  return (a + b);
+  return a + b
 }
 
 function subtract(a, b) {
-  var a = (60);
-  var b = (40);
-  return (a - b);
+  return a - b
 }
 
 function multiply(a, b) {
-  var a = (2);
-  var b = (3.4);
-  return (a * b);
+  return a * b
 }
 
 function divide(a, b) {
-  var a = (5.0);
-  var b = (2.5);
-  return (a / b);
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
