779b03b2ffd53d81827c077cd6bba3f6829a007c
Victoria Chiu <victoria@chiucompany.com>
2016-09-22 00:11:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  var a = (1);
+  var b = (80);
+  return (a + b);
+}
+
+function subtract(a, b) {
+  var a = (60);
+  var b = (40);
+  return (a - b);
+}
+
+function multiply(a, b) {
+  var a = (2);
+  var b = (3.4);
+  return (a * b);
+}
+
+function divide(a, b) {
+  var a = (5.0);
+  var b = (2.5);
+  return (a / b);
+}
