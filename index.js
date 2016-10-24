8b69d0c3e7052ede175dcaffeefeeefc6801b8fd
Jenna Hegarty <jmghga@gmail.com>
2016-10-24 17:14:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(number1,number2){
+  return number1 + number2
+}
+
+function subtract(number1,number2){
+  return number1 - number2
+}
+
+function multilpy(number1,number2){
+  return number1*number2
+}
+
+function divide(number1, number2){
+  return number1/number2
+}
+
+function inc(n){
+  return n+=
+}
+function dec(n){
+  return n-=
+}
