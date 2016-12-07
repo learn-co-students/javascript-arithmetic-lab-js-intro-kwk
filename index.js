f935ac54a83139ce6690d29a378c386c18c61d10
Anjana Nair <anju740@gmail.com>
2016-12-07 03:52:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add (a, b) {
+  var answer = a + b
+  return answer
+}
+
+function subtract (a, b) {
+  var answer = a - b
+  return answer
+}
+
+function multiply (a, b) {
+  var answer = a*b
+  return answer
+}
+
+function divide (a, b) {
+  var answer = a/b
+  return answer
+}
+
+function inc (n) {
+  n++
+  return n
+}
+
+function dec (n) {
+  n--
+  return n
+}
+
+function makeInt (n) {
+  var result = parseInt(n, 10)
+  return result
+}
+
+function preserveDecimal (n) {
+  var result = parseFloat(n)
+  return result
+}
