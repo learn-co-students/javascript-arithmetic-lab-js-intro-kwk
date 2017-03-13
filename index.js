5a96c2997c2eea4d90e38b22ef1ecf2ea5f1561d
Fakhteh Saadatniaki <fakhteh.saadatniaki@tufts.edu>
2017-03-13 04:12:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b){
+  return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(n){
+  n++
+  return n
+}
+function dec(m){
+  m--
+  return m
+}
+function makeInt(str){
+  return parseInt(str,10)
+}
+function preserveDecimal(str){
+  return parseFloat(str)
+}
